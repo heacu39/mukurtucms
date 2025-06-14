44ea8e28a9e6a2a9329054c6ae6c3bf15160603c
Edward Garrett <eg3p@edwards-mini-3.home>
2025-06-14 10:58:55 -0400

    add scald_bilibili module

---
+++ sites/all/modules/custom/scald_bilibili/scald_bilibili_player.tpl.php
@@ -0,0 +1,8 @@
+	<?php
+
+/**
+ * @file
+ * Default theme implementation for Scald Bilibili player.
+ */
+?>
+<iframe title="<?php print $vars['title'] ?>" src="//player.bilibili.com/player.html?aid=<?php print $vars['video_id'] ?>&bvid=<?php print $vars['video_id'] ?>&high_quality=1&autoplay=false" frameborder="no" scrolling="no" allowFullScreen="true"></iframe>
