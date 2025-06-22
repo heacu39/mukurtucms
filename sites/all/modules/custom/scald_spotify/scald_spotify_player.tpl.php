5b5670e0b9fbfab7f6c5a9ca3017c6d5efa85e0f
Edward Garrett <189803938+heacu39@users.noreply.github.com>
2025-06-22 11:12:26 -0400

    more changes

--- sites/all/modules/custom/scald_spotify/scald_spotify_player.tpl.php
+++ sites/all/modules/custom/scald_spotify/scald_spotify_player.tpl.php
@@ -5,4 +5,4 @@
  * Default theme implementation for Scald Spotify player.
  */
 ?>
-<iframe style="border-radius:12px" title="<?php print $vars['title'] ?>" src="https://open.spotify.com/embed/track/<?php print $vars['video_id'] ?>?utm_source=generator" width="101%" height="352" frameborder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+<iframe style="border-radius:12px" title="<?php print $vars['title'] ?>" src="https://open.spotify.com/embed/track/<?php print $vars['video_id'] ?>?utm_source=generator" frameborder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
