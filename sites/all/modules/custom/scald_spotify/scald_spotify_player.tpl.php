e88b8fc618a7935383b3e9f506029fbb626c2591
Edward Garrett <189803938+heacu39@users.noreply.github.com>
2025-06-22 11:08:09 -0400

    add scald_spotify module first version

---
+++ sites/all/modules/custom/scald_spotify/scald_spotify_player.tpl.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @file
+ * Default theme implementation for Scald Spotify player.
+ */
+?>
+<iframe style="border-radius:12px" title="<?php print $vars['title'] ?>" src="https://open.spotify.com/embed/track/<?php print $vars['video_id'] ?>?utm_source=generator" width="101%" height="352" frameborder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
