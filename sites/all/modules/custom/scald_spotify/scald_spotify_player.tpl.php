a5a1cbf4a9b9fdb2f519217243b44de00cf0262c
Edward Garrett <189803938+heacu39@users.noreply.github.com>
2025-07-16 09:01:55 +0100

    support spotify podcast episodes in addition to spotify tracks

--- sites/all/modules/custom/scald_spotify/scald_spotify_player.tpl.php
+++ sites/all/modules/custom/scald_spotify/scald_spotify_player.tpl.php
@@ -5,4 +5,4 @@
  * Default theme implementation for Scald Spotify player.
  */
 ?>
-<iframe style="border-radius:12px" title="<?php print $vars['title'] ?>" src="https://open.spotify.com/embed/track/<?php print $vars['video_id'] ?>?utm_source=generator" frameborder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+<iframe style="border-radius:12px" title="<?php print $vars['title'] ?>" src="https://open.spotify.com/embed/<?php print $vars['video_id'] ?>?utm_source=generator" frameborder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
